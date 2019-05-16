49e0d89f0a97e424ba3ad970402a6d0f19582407
mvargasvega <martinvargasvega@gmail.com>
2019-05-16 16:18:41 -0700

    Added jquery event for mobile nav links. Changed routes to links for web pages

--- js/script.js
+++ js/script.js
@@ -1,13 +1,5 @@
 console.log("Sanity Check");
 
-
-document.querySelector('.red').addEventListener('click',turnRed)
-
-function turnRed(){
-  this.style.backgroundColor = "red";
-  console.log('Button pressed');
-  var para = document.createElement("P");
-  var t = document.createTextNode("This is a new p tag");
-  para.appendChild(t);
-  document.getElementById("add").appendChild(para)
-}
+$( document ).ready(function(){
+  $('.sidenav').sidenav();
+});
